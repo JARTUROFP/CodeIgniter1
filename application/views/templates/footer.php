2234cb2b52cce4fb3e06ef05d3c61df4f1bb8028
JARTUROFP <agar_mexi14_@hotmail.com>
2020-03-31 16:50:28 -0600

    Initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,54 @@
+<br>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
+  Botón modal
+</button>
+<br>
+<br>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">Tenencia</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+¿En qué consiste el subsidio a la Tenencia 2020?
+Si eres persona física o jurídica colectiva no lucrativa y tu vehículo tiene un valor factura menor a 400 mil pesos sin IVA para autos, y un valor factura menor a 115 mil pesos sin IVA para motocicletas, y te encuentras al corriente, o bien, adeudas el ejercicio fiscal 2019 y te pones al corriente, paga sólo el refrendo.
+
+¿Cuándo no es procedente el subsidio a la Tenencia 2020?
+
+Cuando los pagos de derechos por servicios de control vehicular se realicen con posterioridad al 31 de marzo.
+Los vehículos que provengan de otra entidad.
+Los contribuyentes propietarios de vehículos de servicio particular con placas de circulación expedidas en el ejercicio fiscal 2013 y anteriores que no hayan cumplido con la obligación de realizar la renovación de placas durante el periodo de julio a diciembre del año 2019.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerca</button>
+        <button type="button" class="btn btn-primary">Guardar cambios</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<section>
+	
+
+
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action active">
+    Cras justo odio
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+  <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+  <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
+</div>
+
+</section>
+
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+
+<style>
+  #container {
+    margin: 10px;
+    border: 5px solid #00B140;
+    box-shadow: 0 0 8px #D0D0D0;
+  }
+  </style>
+
+  <body>
+<nav style="background-color: #EFEFEF;" id="container" class="navbar navbar-expand-lg navbar-light bg-ligh">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Enlace</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Desplegable
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Acción</a>
+          <a class="dropdown-item" href="#">Otra accion</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Algo mas aqui</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Discapacitado</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="http://caci.ochentayocho.net/public/frontend/imagenes/banner1.jpg" class="d-block w-100" alt="Los Angeles">
+    </div>
+    <div class="carousel-item">
+      <img src="http://caci.ochentayocho.net/public/frontend/imagenes/banner2.jpg" class="d-block w-100" alt="Chicago">
+    </div>
+    <div class="carousel-item">
+      <img src="http://caci.ochentayocho.net/public/frontend/imagenes/banner3.jpg" class="d-block w-100" alt="New york">
+    </div>
+  </div>
+</div>
+<section><section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"> 
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
